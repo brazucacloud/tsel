1e627c62fd50731b2922ab2d781b7777665db005
brazucacloud <alvaro@brazucacloud.com.br>
2025-08-06 04:36:38 -0300

    🔧 Corrige importação do middleware auth em reports.js

--- routes/reports.js
+++ routes/reports.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const Task = require('../models/Task');
 const Device = require('../models/Device');
 const moment = require('moment');
